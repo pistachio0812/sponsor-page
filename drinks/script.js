97956c7aa56abac4708b8d206600e0dbec3332fc
Jakob <65709175+pistachio0812@users.noreply.github.com>
2022-10-15 19:00:06 +0800

    Update script.js

--- drinks/script.js
+++ drinks/script.js
@@ -11,7 +11,7 @@ var drinks_qrcodes = $('#drinks-qrcodes');
 var drinks_qrcode = $('#drinks-qrcode');
 var isMobile = /Android|webOS|iPhone|iPad|BlackBerry/i.test(navigator.userAgent);
 
-var GithubLink  =   "https://github.com/kelecn/sponsor-page/";
+var GithubLink  =   "https://github.com/pistachio0812/sponsor-page/";
 var PayPalLink  =   "https://www.paypal.me/kelecn";
 $('#paypal-donate>a').href = PayPalLink;
 $('#github-box>a').href = GithubLink;
